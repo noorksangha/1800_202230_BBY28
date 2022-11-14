0a9012f7f70305fa7c40888962350fca3629cf36
Noor Sangha <noorsangh02@gmail.com>
2022-11-13 19:21:44 -0800

    attempt to add data.

---
+++ scripts/setGoal.js
@@ -0,0 +1,34 @@
+function writeGoal() {
+    console.log("in")
+    let GoalName = document.getElementById("validationName").value;
+    let Deadline = document.getElementById("validationDeadline").value;
+    let Amount = document.getElementId("validationAmount").value;
+    let Notes = document.getElementId("validationDefault03").value;
+    console.log(GoalName, Deadline, Amount, Notes);
+
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid)
+        var userID = user.uid;
+        //get the docuement for current user.
+        currentUser.get()
+            .then(userDoc => {
+                var userEmail = userDoc.data().email;
+                db.collection("Goals").add({
+                    userID: userID,
+                    validationName: GoalName,
+                    validationDeadline: Deadline,
+                    validationAmount: Amount,
+                    validationDefault03: Notes,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                }).then(()=>{
+                    window.location.href = "thank.html";
+                })
+
+                })
+            } else {
+                //No user is signed in.
+            }
+    });
+}
