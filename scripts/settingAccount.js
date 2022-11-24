503277e1bfc11fc03cc0b62561b84edd9b176e94
Quan Tran <caoquantran2004@gmail.com>
2022-11-24 12:13:23 -0800

    fixed some settingAccount

--- scripts/settingAccount.js
+++ scripts/settingAccount.js
@@ -1,49 +1,44 @@
-function incomeHandler () {
-    console.log("bruh");
-    if (document.getElementById('hourly-income').checked){
-        document.getElementById('exampleFormControlInput1').disabled = false;
-        document.getElementById('exampleFormControlInput2').disabled = false;
-    } else {
-        document.getElementById('exampleFormControlInput1').disabled = true;
-        document.getElementById('exampleFormControlInput2').disabled = true;
-    }
+function incomeHandler() {
+  console.log("bruh");
+  if (document.getElementById("hourly-income").checked) {
+    document.getElementById("exampleFormControlInput1").disabled = false;
+    document.getElementById("exampleFormControlInput2").disabled = false;
+  } else {
+    document.getElementById("exampleFormControlInput1").disabled = true;
+    document.getElementById("exampleFormControlInput2").disabled = true;
+  }
 }
 
 function writeIncome() {
-    console.log("in")
-    var income = document.getElementById("incomeAmount").value;
-    let type = "monthly";
-    if (document.getElementById('hourly-income').checked) {
-        var days = document.getElementById("exampleFormControlInput2").value;
-        var hours = document.getElementById("exampleFormControlInput1").value;
-        income = income * hours * days;
+  console.log("in");
+  var income = document.getElementById("incomeAmount").value;
+  let type = "monthly";
+  if (document.getElementById("hourly-income").checked) {
+    var days = document.getElementById("exampleFormControlInput2").value;
+    var hours = document.getElementById("exampleFormControlInput1").value;
+    income = income * hours * days;
+  }
+  if (document.getElementById("monthly-income").checked) {
+    type = "monthly";
+  }
+  if (document.getElementById("yearly-income").checked) {
+    income = income * 12;
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user.uid);
+      //var currentUser = db.collection("users").doc(user.uid);
+      //get the docuement for current user.
+      db.collection("users")
+        .doc(user.uid)
+        .update({
+          income: income
+        }).then(()=>(window.location.href = "mainpage.html"));
+        console.log("added");
+        //.then(()=>(window.location.href = "mainpage.html"));
+    } else {
+      //No user is signed in.
     }
-    if (document.getElementById('monthly-income').checked){
-        type = "monthly";
-    }
-    if (document.getElementById('yearly-income').checked){
-        income = income * 12;
-    }
-
-
-
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log("nailed");
-            var currentUser = db.collection("users").doc(user.uid)
-            //get the docuement for current user.
-            currentUser.get()
-                .then(() => {
-                    console.log("added");
-                    db.collection("users").doc(user.uid).update({
-                        income: income
-                    });
-                    }).then(
-                        window.location.href = "mainpage.html"
-                    );
-
-        } else {
-            //No user is signed in.
-        }
-    });
+  });
 }
