32566d68f4ce51fd746f7213553edee8b50ab6d2
Kenneth Tran <tlekhang40@gmail.com>
2022-11-24 12:21:40 -0800

    settingAccount fixed

--- scripts/settingAccount.js
+++ scripts/settingAccount.js
@@ -11,18 +11,18 @@ function incomeHandler() {
 
 function writeIncome() {
   console.log("in");
-  var income = document.getElementById("incomeAmount").value;
+  var income2 = document.getElementById("incomeAmount").value;
   let type = "monthly";
   if (document.getElementById("hourly-income").checked) {
     var days = document.getElementById("exampleFormControlInput2").value;
     var hours = document.getElementById("exampleFormControlInput1").value;
-    income = income * hours * days;
+    income2 = income2 * hours * days;
   }
   if (document.getElementById("monthly-income").checked) {
     type = "monthly";
   }
   if (document.getElementById("yearly-income").checked) {
-    income = income * 12;
+    income2 = income2 * 12;
   }
 
   firebase.auth().onAuthStateChanged((user) => {
@@ -33,7 +33,7 @@ function writeIncome() {
       db.collection("users")
         .doc(user.uid)
         .update({
-          income: income
+          income: income2
         }).then(()=>(window.location.href = "mainpage.html"));
         console.log("added");
         //.then(()=>(window.location.href = "mainpage.html"));
